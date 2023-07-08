5f9c12896fee16d71e38c5e901f8e46963ac6d04
ShiZheng <ShiZhengWork@outlook.com>
2023-07-08 17:46:53 +0800

    sad

--- 文章切割器-ChatGPT/index.js
+++ 文章切割器-ChatGPT/index.js
@@ -38,8 +38,19 @@ fs.readFile('./file.txt', 'utf8', (err, data) => {
 
   // 将每个数组元素写入单独的txt文件
   result.forEach((chunk, index) => {
+    const newChunk = `
+    你好，你是一个长文章总结小助手，我会将我的一篇长文章发给你，你需要做的事情是提炼其中的核心观点，然后将其发给我
+    注意：
+    请使用 Markdown 语言，
+    请按照 Markdownlint 的规则给我返回值
+    请在你认为觉得重要的地方加粗
+
+
+    请总结一下内容：
+    ${chunk}
+    `
     const filename = path.join(outputDir, `output_${index + 1}.txt`);
-    fs.writeFile(filename, chunk, 'utf8', (err) => {
+    fs.writeFile(filename, newChunk, 'utf8', (err) => {
       if (err) {
         console.error(`写入文件 ${filename} 时发生错误：`, err);
         return;
